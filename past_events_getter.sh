782e7c080b08566642b85c6ad2c62301a9916651
Erkan Erol <erkan.erol@sap.com>
2018-05-27 19:09:08 +0300

    fix meetup urls

--- past_events_getter.sh
+++ past_events_getter.sh
@@ -1,1 +1,3 @@
-curl 'https://api.meetup.com/Software-Craftsmanship-Turkey/events?&sign=true&photo-host=public&page=100&status=past'  | jq -r  'reverse | .[] | ("* ["+.local_date+" "+.name+"](https://www.meetup.com/Software-Craftsmanship-Turkey/events/248799824/"+.id+")")' > past_meetups.md
+curl 'https://api.meetup.com/Software-Craftsmanship-Turkey/events?&sign=true&photo-host=public&page=100&status=past'  | jq -r  'reverse | .[] | ("* ["+.local_date+" "+.name+"](https://www.meetup.com/Software-Craftsmanship-Turkey/events/"+.id+")")' > past_meetups.md
+
+
